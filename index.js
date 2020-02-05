1cd40cec02a39a8c97629213c5ef0698e47da278
Marek Kobyliński <Marecki1121@gmail.com>
2020-02-05 23:45:37 +0100

    feat: recognize captcha and send to service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
 const puppeteer = require('puppeteer');
+const fetch = require("node-fetch");
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ['--start-fullscreen']
+    slowMo: 30,
   });
   const page = await browser.newPage();
   await page.setViewport({
@@ -23,15 +25,54 @@ const puppeteer = require('puppeteer');
   await page.screenshot({ path: '1dateChapter.png' });
   await page.click('div[class="next-button"]');
   await page.waitForSelector('input[name="username"]', { visible: true, timeout: 0 });
-  await page.focus('input[name=username]');
-  await page.keyboard.type('algorytm86');
-  await page.focus('input[name=password]');
-  await page.keyboard.type('algorytm86123');
-  await page.focus('input[name=confirm_password]');
-  await page.keyboard.type('algorytm86123');
+  await page.type('input[name=username]', 'algorytm86');
+  await page.type('input[name=password]', 'algorytm86123');
+  await page.type('input[name=confirm_password]', 'algorytm86123');
   await page.click('div[class=checkbox-indicator]');
   await page.screenshot({ path: '1userNameChapter.png' });
   await page.click('div[class="next-button"]');
   await page.screenshot({ path: '1recapcha.png' });
-  await browser.close();
-})();
+  let id = 0;
+  await fetch("https://2captcha.com/in.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      key: "d8165f7c75664ca75b4d0a63fe113190",
+      method: "userrecaptcha",
+      googlekey: "6Lc3HAsUAAAAACsN7CgY9MMVxo2M09n_e4heJEiZ",
+      pageurl: "https://signup.euw.leagueoflegends.com/en/signup/index#/registration",
+      json: "1"
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      id = data.request
+    })
+    .catch(err => console.error(err))
+
+  //page.waitFor(10000);
+  console.log(id)
+  console.log(`https://2captcha.com/res.php?key=d8165f7c75664ca75b4d0a63fe113190&action=get&id=${id}&json=1`)
+  let token;
+  while (token != "CAPCHA_NOT_READY") {
+    await fetch(`https://2captcha.com/res.php?key=d8165f7c75664ca75b4d0a63fe113190&action=get&id=${id}&json=1`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.request)
+        if (data.request === 'CAPCHA_NOT_READY') {
+          console.log(data.request + 2)
+        } token = data.request
+        console.log(data.request + 3)
+      })
+      .catch(err => console.error(err))
+  }
+  await page.screenshot({ path: 'response.png', fullPage: true })
+  //await browser.close()
+})()
