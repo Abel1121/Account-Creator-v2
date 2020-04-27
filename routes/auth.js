28c4ad429b0905a6fc76f2d22c908a119501a27e
Marek Kobyliński <Marecki1121@gmail.com>
2020-04-27 21:37:21 +0200

    add: validation for register and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation, loginValidation }= require('../validation.js');
+
 
 router.post('/register', async (req, res) => {
+
+    const { error } = registerValidation(req.body); // this lane validate body
+    if ( error ) return res.status(400).send(error.details[0].message) //what is wrong 
+
     const user = new User({
         username: req.body.username,
         email: req.body.email,
@@ -10,8 +16,8 @@ router.post('/register', async (req, res) => {
     try { 
         const savedUser = await user.save();
         res.send(savedUser);
-    } catch (err) {
-        res.status(400).send(err)
+    }catch (err) {
+        res.status(400).send(err);
     }
 })
 
