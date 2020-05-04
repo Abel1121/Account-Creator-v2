56b4186136fe8ce0b5ee5d2f5e4669de7d5d8acd
Marek Kobyliński <Marecki1121@gmail.com>
2020-05-04 20:09:29 +0200

    add: login and register feture

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const { registerValidation, loginValidation }= require('../validation.js');
 
-
+//Register
 router.post('/register', async (req, res) => {
 
+        //Validate data before we a user
     const { error } = registerValidation(req.body); // this lane validate body
     if ( error ) return res.status(400).send(error.details[0].message) //what is wrong 
 
+        //checking if the user already exist
+    const emailExist = await User.findOne({email: req.body.email})
+    if (emailExist) return res.status(400).send('Email already exists');
+
+        // HASHing the paswword
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
     const user = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     })
     try { 
         const savedUser = await user.save();
@@ -19,6 +30,26 @@ router.post('/register', async (req, res) => {
     }catch (err) {
         res.status(400).send(err);
     }
+})
+
+//Login
+router.post('/login', async (req, res) => {
+
+     const { error } = loginValidation(req.body); // this lane validate body
+    if (error) return res.status(400).send(error.details[0].message);
+    
+       //checking if the email already exist
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email not found');
+
+        //Pasword is Correct
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid Password');
+    
+        //Create a JWT
+    const token = jwt.sign({_id: user._id})
+
+    res.send('Login is correct')
 })
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -14,7 +14,7 @@ const registerValidation = (data) => {
 
 const loginValidation = (data) => {
     const schema = Joi.object({
-        username: Joi.string().min(5).required(),
+        email: Joi.string().min(6).required(),
         password: Joi.string().min(6).required()
     })
     return schema.validate(data)
